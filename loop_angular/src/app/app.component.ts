6ae0a08993eac6cff97f4e49c043038d9ba957bd
Vaibhav Varade <vaibhav.varade@splendidinfosystems.com>
2023-11-24 09:35:49 +0530

    Angular

---
+++ loop_angular/src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl:'./app.component.html' ,
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'loop_angular';
+  divCount: any;
+results: any;
+any: any;
+// users = ['Vaibhav','Varade','Dinesh','Patil','Mayur','Varade'];
+}
+export class AppComponent2 {
+  divCount = 5 ; 
+}
+// export class AppComponent3 {
+//   divCount = 5 ; 
+// }
+
+
